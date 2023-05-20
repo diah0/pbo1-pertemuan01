78c5cf61fa6eddb97d3736e03848c3a64a78f516
diah0 <halimatus833@gmail.com>
2023-05-20 11:44:00 +0800

    Hello Git & Java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
     public static void main (String [] args){
-        System.out.println("Hello World !!");
+        System.out.println("Hello Git !!");
+        System.out.println("Hello Java!!");
     }
 }
